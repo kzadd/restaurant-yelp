62951cdcc536a4267356c0515744f5bd23d7c181
Ricardo Carrasquero <ricardo.carrasquero@chilecompra.cl>
2023-08-19 16:12:18 -0400

    feat: add package test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
@@ -6,5 +8,6 @@ export default defineConfig({
   plugins: [react()],
   preview: { port: 3030 },
   resolve: { alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }] },
-  server: { port: 3000 }
+  server: { port: 3000 },
+  test: { environment: 'jsdom' }
 })
